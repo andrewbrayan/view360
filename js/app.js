639e9738c5d8f3fc6df0eb6cc63f8363a6e70097
Brayan Martinez Lemos <Bmartinez@econtainers.local>
2022-03-26 12:16:28 -0500

    cambios

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent("pick", {
   init: function () {
     this.el.addEventListener("click", () => {
-      document.querySelector('#infoPanel').setAttribute('visible', 'true')
+      document.querySelector("#infoPanel").setAttribute("visible", "true");
     });
   },
 });
@@ -9,8 +9,23 @@ AFRAME.registerComponent("pick", {
 AFRAME.registerComponent("close", {
   init: function () {
     this.el.addEventListener("click", () => {
-      document.querySelector('#infoPanel').setAttribute('visible', 'false')
+      document.querySelector("#infoPanel").setAttribute("visible", "false");
     });
   },
 });
 
+window.addEventListener("wheel", (event) => {
+  const delta = Math.sign(event.wheelDelta)/10;
+  console.log('rueda',delta);
+  //getting the mouse wheel change (120 or -120 and normalizing it to 1 or -1)
+  var myZoom = document.getElementById("camera").getAttribute("zoom");
+  console.log('myZoom',myZoom);
+  var finalZoom = parseFloat(myZoom) + delta;
+  console.log('final zoom',finalZoom);
+  //limiting the zoom so it doesnt zoom too much in or out
+  if (finalZoom < 1) finalZoom = 1;
+  if (finalZoom > 5) finalZoom = 5;
+
+  //setting the camera element
+  document.getElementById("camera").setAttribute("zoom", finalZoom);
+});
