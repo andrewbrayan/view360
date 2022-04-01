2d19ed9f2c3c4d9c6b7d2d1aed262d04070b2b58
Brayan Martinez Lemos <Bmartinez@econtainers.local>
2022-04-01 10:58:48 -0500

    complentando vista oficina sencilla

--- oficina-full/js/app-of.js
+++ oficina-full/js/app-of.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent("change", {
 
         changePosition("raycast", "aireAcondicionado", "5 4.5 -18");
         changePosition("raycast", "accessories", "7 -1.5 -18");
-        changePosition("raycast", "superBoard", "9 4.8 -18");
+        changePosition("raycast", "ceiling", "9 4.8 -18");
         changePosition("raycast", "electricDist", "-5 -7.2 15", "0 -25 0");
         changePosition("raycast", "floorVin", "1 -8 10");
         changePosition("raycast", "doorWind", "10 -2.5 10", "0 60 0");
@@ -21,7 +21,7 @@ AFRAME.registerComponent("change", {
 
         changePosition("raycast", "aireAcondicionado", "2.5 5.3 -15");
         changePosition("raycast", "accessories", "5 -3.5 -15");
-        changePosition("raycast", "superBoard", "7 6 -15");
+        changePosition("raycast", "ceiling", "7 6 -15");
         changePosition("raycast", "electricDist", "-6.7 -2.2 15");
         changePosition("raycast", "floorVin", "-3.5 -5.5 15");
         changePosition("raycast", "doorWind", "0.5 0 15");
@@ -42,3 +42,14 @@ function changePosition(id, infoCard, position, rotation) {
     }
   });
 }
+
+codeAlpha = "en";
+fetch("../assets/text.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsondata) => {
+    console.log('holaa');
+    document.getElementById("navTitle").innerHTML =
+      jsondata[codeAlpha].oficinaFull.navTitle;
+  });
