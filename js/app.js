4d62e39dd46d1f25fa3d7d7ea0293aed59d9b09e
Brayan Martinez Lemos <Bmartinez@econtainers.local>
2022-04-05 15:56:22 -0500

    proyecto finalizado espera de solicitud de cambios

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 if (document.getElementById("cardModal")) {
   var myModal = new bootstrap.Modal(document.getElementById("cardModal"), {
     keyboard: true,
-  });  
+  });
 }
 
 // funcion de A-frame para la creacion de atributos personales.
@@ -29,29 +29,53 @@ AFRAME.registerComponent("close", {
   },
 });
 
+mouseOver = false;
+window.addEventListener("mouseover", (event) => {
+  if (event.target.localName == "canvas") {
+    mouseOver = true;
+  } else {
+    mouseOver = false;
+  }
+});
+
 window.addEventListener("wheel", (event) => {
-  const delta = Math.sign(event.wheelDelta) / 10;
+  if (mouseOver) {
+    const delta = Math.sign(event.wheelDelta) / 10;
 
-  var myZoom = document.getElementById("camera").getAttribute("zoom");
-  var finalZoom = parseFloat(myZoom) + delta;
+    var myZoom = document.getElementById("camera").getAttribute("zoom");
+    var finalZoom = parseFloat(myZoom) + delta;
 
-  if (finalZoom < 1) finalZoom = 1;
-  if (finalZoom > 5) finalZoom = 5;
+    if (finalZoom < 1) finalZoom = 1;
+    if (finalZoom > 5) finalZoom = 5;
 
-  document.getElementById("camera").setAttribute("zoom", finalZoom);
+    document.getElementById("camera").setAttribute("zoom", finalZoom);
+  }
 });
 
 codeAlpha = "es";
 
 // funcion Geolocalizacion
-fetch("https://us-central1-econtainers2019.cloudfunctions.net/geojs-country").then((response) => {
-  return response.json();
-}).then((data) => {
-  console.log(data);
-  if (data.country == "US") {
-    codeAlpha = "en";
-  }
-})
+fetch("https://us-central1-econtainers2019.cloudfunctions.net/geojs-country")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    if (data.country == "US") {
+      codeAlpha = "en";
+    }
+
+    fetch("../assets/text.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsondata) => {
+        menuList = document.querySelectorAll(".textMenu");
+        menuList.forEach((element) => {
+          params = element.id.split("-");
+          element.innerHTML = jsondata[codeAlpha][params[0]][params[1]];
+        });
+      });
+  });
 
 // funcion para obtener data de Json
 function setInfoCard(cardType, panorama) {
